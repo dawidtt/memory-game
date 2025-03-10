67060b3447013cccad95cf7e34f522963a904ca1
dawidtt <dawid.gorszka@gmail.com>
2025-03-10 19:03:20 +0100

    Make pokemons in cards to be random

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,55 @@ import WinModal from "./WinModal";
 
 function App() {
   const [isGameOver, setIsGameOver] = useState(false);
+  function generatePokemons() {
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+    const pokemonsIds = [];
+    const pokemons = [];
+
+    for (let i = 0; i < 8; i++) {
+      while (true) {
+        const randomNum = Math.ceil(Math.random() * 100);
+        if (!pokemonsIds.includes(randomNum)) {
+          pokemonsIds.push(randomNum);
+          break;
+        }
+      }
+    }
+    const pokemonsIdsMultiplied = [...pokemonsIds, ...pokemonsIds];
+    for (let i = 0; i < 16; i++) {
+      pokemons.push({
+        fetchId: pokemonsIdsMultiplied[i],
+        id: crypto.randomUUID(),
+        isShown: false,
+        isSelected: false,
+        wasSelected: false,
+      });
+    }
+    shuffleArray(pokemons);
+    return pokemons;
+  }
+
   function gameOver() {
     setIsGameOver(true);
   }
+
+  const [pokemons, setPokemons] = useState(generatePokemons());
+
   return (
     <>
       <header>
         <h1>Pokemon Memory Game</h1>
       </header>
-      <Gameboard gameOver={gameOver} />
+      <Gameboard
+        gameOver={gameOver}
+        pokemons={pokemons}
+        setPokemons={setPokemons}
+      />
       {isGameOver && <WinModal />}
     </>
   );
